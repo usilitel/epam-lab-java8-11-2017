21d873c937bf076f9f49bc3e3e59e375d0adacfa
usilitel <usilitel00@gmail.com>
2017-11-19 11:06:23 +0300

    Update Exercise1.java

--- src/test/java/lambda/part1/exercise/Exercise1.java
+++ src/test/java/lambda/part1/exercise/Exercise1.java
@@ -28,10 +28,10 @@ public class Exercise1 {
         Arrays.sort(persons, comparatorByAge);
 
         assertArrayEquals(new Person[]{
-                new Person("Иван", "Мельников", 20),
-                new Person("Николай", "Зимов", 30),
-                new Person("Алексей", "Доренко", 40),
-                new Person("Артем", "Зимов", 45)
+            new Person("Иван", "Мельников", 20),
+            new Person("Николай", "Зимов", 30),
+            new Person("Алексей", "Доренко", 40),
+            new Person("Артем", "Зимов", 45)
         }, persons);
     }
 
@@ -47,10 +47,10 @@ public class Exercise1 {
         });
 
         assertArrayEquals(new Person[]{
-                new Person("Иван", "Мельников", 20),
-                new Person("Николай", "Зимов", 30),
-                new Person("Алексей", "Доренко", 40),
-                new Person("Артем", "Зимов", 45)
+            new Person("Иван", "Мельников", 20),
+            new Person("Николай", "Зимов", 30),
+            new Person("Алексей", "Доренко", 40),
+            new Person("Артем", "Зимов", 45)
         }, persons);
     }
 
@@ -61,22 +61,19 @@ public class Exercise1 {
         Arrays.sort(persons, new Comparator<Person>() {
             @Override
             public int compare(Person left, Person right) {
-                return left.getFirstName().compareTo(right.getFirstName());
+                int lastNameCompare = left.getLastName().compareTo(right.getLastName());
+                if (lastNameCompare == 0)
+                    return left.getFirstName().compareTo(right.getFirstName());
+                else
+                    return lastNameCompare;
             }
-        });
-
-        Arrays.sort(persons, new Comparator<Person>() {
-            @Override
-            public int compare(Person left, Person right) {
-                return left.getLastName().compareTo(right.getLastName());
-            }
-        });
-
+        });        
+        
         assertArrayEquals(new Person[]{
-                new Person("Алексей", "Доренко", 40),
-                new Person("Артем", "Зимов", 45),
-                new Person("Николай", "Зимов", 30),
-                new Person("Иван", "Мельников", 20)
+            new Person("Алексей", "Доренко", 40),
+            new Person("Артем", "Зимов", 45),
+            new Person("Николай", "Зимов", 30),
+            new Person("Иван", "Мельников", 20)
         }, persons);
     }
 
@@ -122,10 +119,10 @@ public class Exercise1 {
 
     private Person[] getPersons() {
         return new Person[]{
-                new Person("Иван", "Мельников", 20),
-                new Person("Алексей", "Доренко", 40),
-                new Person("Николай", "Зимов", 30),
-                new Person("Артем", "Зимов", 45)
+            new Person("Иван", "Мельников", 20),
+            new Person("Алексей", "Доренко", 40),
+            new Person("Николай", "Зимов", 30),
+            new Person("Артем", "Зимов", 45)
         };
     }
 }
